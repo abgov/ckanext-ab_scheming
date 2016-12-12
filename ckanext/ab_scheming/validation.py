1827704c76229d5f2f1db9fe3eb1b4c86014c5ba
Yongjie Liang <jack.liang@gov.ab.ca>
2016-12-12 13:30:17 -0700

    Update validator to include field's choices. Change to custom validators for fields topics and creator in opendata.json and publications.json

--- ckanext/ab_scheming/validation.py
+++ ckanext/ab_scheming/validation.py
@@ -2,6 +2,7 @@ import ckanext.ab_scheming.helpers as helpers
 from ckan.lib.navl.dictization_functions import unflatten
 from ckantoolkit import missing
 import json
+from ckan.common import _
 
 def scheming_validator(fn):
     """
@@ -27,7 +28,14 @@ def ab_scheming_multiple_choice(field, schema):
 
        "choice-a"
     """
-    choice_values = set(c['title'] for c in helpers.topics_available() )
+    if field.get('choices'):
+        source = field['choices']
+        key_need = 'value'
+    else:
+        source = helpers.topics_available()
+        key_need = 'title'
+
+    choice_values = set(c[key_need] for c in source )
 
     def validator(key, data, errors, context):
         # if there was an error before calling our validator
@@ -53,9 +61,9 @@ def ab_scheming_multiple_choice(field, schema):
 
         if not errors[key]:
             data[key] = json.dumps([
-                c['title'] for c in helpers.topics_available() if c['title'] in selected])
-
-            if field.get('required') and not selected:
-                errors[key].append(_('Select at least one'))
+                c[key_need] for c in source if c[key_need] in selected])
+  
+            #if field.get('required') and not selected:
+            #    errors[key].append(_('Select at least one'))
 
     return validator
