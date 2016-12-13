282eb63685d872bd2aaa8e734d2070dd96a5354a
Yongjie Liang <jack.liang@gov.ab.ca>
2016-12-13 09:46:02 -0700

    Cleanup get.py

--- ckanext/ab_scheming/logic/action/get.py
+++ ckanext/ab_scheming/logic/action/get.py
@@ -15,23 +15,8 @@ def topics_list_for_user(context, data_dict):
     admin of.
 
     Specifically it returns the list of all types of groups that the currently
-    authorized user has a given permission (for example: "manage_group") against.
-
-    When a user becomes a member of a group in CKAN they're given a
-    "capacity" (sometimes called a "role"), for example "member", "editor" or
-    "admin".
-
-    Each of these roles has certain permissions associated with it. For example
-    the admin role has the "admin" permission (which means they have permission
-    to do anything). The editor role has permissions like "create_dataset",
-    "update_dataset" and "delete_dataset".  The member role has the "read"
-    permission.
-
-    This function returns the list of all types of groups that the authorized user
-    has a given permission for. For example the list of all types of groups that the
-    user is an admin of, or the list of all types of groups that the user can create
-    datasets in. This takes account of when permissions cascade down an
-    all types of groups hierarchy.
+    authorized user has a given create permission (for example: "manage_group") 
+    against.
 
     :param permission: the permission the user has against the
         returned organizations, for example ``"read"`` or ``"create_dataset"``
@@ -51,45 +36,7 @@ def topics_list_for_user(context, data_dict):
     topics_q = model.Session.query(model.Group) \
         .filter(model.Group.type == "topics") \
         .filter(model.Group.state == 'active')
-    """
-    if not sysadmin:
-        # for non-Sysadmins check they have the required permission
-
-        # NB 'edit_group' doesn't exist so by default this action returns just
-        # orgs with admin role
-        permission = data_dict.get('permission', 'edit_group')
-
-        roles = authz.get_roles_with_permission(permission)
-
-        if not roles:
-            return []
-        user_id = authz.get_user_id_for_username(user, allow_none=True)
-        if not user_id:
-            return []
-
-        q = model.Session.query(model.Member, model.Group) \
-            .filter(model.Member.table_name == 'user') \
-            .filter(model.Member.capacity.in_(roles)) \
-            .filter(model.Member.table_id == user_id) \
-            .filter(model.Member.state == 'active') \
-            .join(model.Group)
-
-        group_ids = set()
-        roles_that_cascade = \
-            authz.check_config_permission('roles_that_cascade_to_sub_groups')
-        for member, group in q.all():
-            if member.capacity in roles_that_cascade:
-                group_ids |= set([
-                    grp_tuple[0] for grp_tuple
-                    in group.get_children_group_hierarchy(type='topics')
-                    ])
-            group_ids.add(group.id)
-
-        if not group_ids:
-            return []
-
-        topics_q = topics_q.filter(model.Group.id.in_(group_ids))
-    """
+    
     orgs_list = model_dictize.group_list_dictize(topics_q.all(), context)
     for o in orgs_list:
         o['title'] = o['title'].decode('utf_8')
