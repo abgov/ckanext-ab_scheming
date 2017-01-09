50ec3bab5f64e0a47551c7ee340fa3491f5c4226
Yongjie Liang <jack.liang@gov.ab.ca>
2017-01-09 12:00:24 -0700

    Fix member_create is logic/auth. Call the ckan core function instead of rewriting it.

--- ckanext/ab_scheming/logic/auth/create.py
+++ ckanext/ab_scheming/logic/auth/create.py
@@ -1,32 +1,14 @@
 # encoding: utf-8
 
-import ckan.logic as logic
-import ckan.authz as authz
 import ckan.logic.auth as logic_auth
-
-from ckan.common import _
-
+from ckan.logic.auth import create
 
 
 def member_create(context, data_dict):
     group = logic_auth.get_group_object(context, data_dict)
-    user = context['user']
-
-    # User must be able to update the group to add a member to it
-    permission = 'update'
-    # However if the user is member of group then they can add/remove datasets
-    if not group.is_organization and data_dict.get('object_type') == 'package':
-        permission = 'manage_group'
 
     if group.type == 'topics':
-        authorized = True
-    else:
-        authorized = authz.has_user_permission_for_group_or_org(group.id,
-                                                                user,
-                                                                permission)
-    if not authorized:
-        return {'success': False,
-                'msg': _('User %s not authorized to edit group %s') %
-                        (str(user), group.id)}
-    else:
         return {'success': True}
+    else:
+        return create.member_create(context, data_dict)
+    
